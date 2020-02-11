9564ac23bd6ec320ba5dacc9de98801ab82f8d67
AlejandroSherman <alex@shermanart.net>
2020-02-11 09:55:41 -0800

    added selection sort files

---
+++ SelectionSort.cpp
@@ -0,0 +1,21 @@
+#ifndef __SELECTIONSORT_CPP
+#define __SELECTIONSORT_CPP
+
+#include "SelectionSort.hpp"
+
+
+void SelctionSort::Sort(Container* container){
+     int i,j,first;
+     int numLength = container.size();
+     for (i = numLength-1; i > 0; i--){
+     first = 0;
+     for(j=1;j<=i;j++){
+       if (container.at(j)->evaluate() < container.at(first)->evaluate)
+       first = j;
+     }
+     container.swap(i,first);
+     }
+    return;
+}
+
+#endif
---
+++ SelectionSort.hpp
@@ -0,0 +1,12 @@
+#ifndef __SELECTIONSORT_HPP
+#define __SELECTIONSORT_HPP
+
+#include "container.hpp"
+#include "sort.hpp"
+
+Class SelectionSort : public Sort{
+private:
+public: 
+    void sort(Container* container);
+};
+#endif
