ba2ac45c9c5b242248124ee018da729a9c1eaaca
Ernie1056 <ernie6105@gmail.com>
2020-02-11 10:23:19 -0800

    Modified cmake and vcon test

--- VectorContainer.hpp
+++ VectorContainer.hpp
@@ -2,11 +2,12 @@
 #define _VECTORCONTAINER_H_
 #include "container.hpp"
 #include <vector>
+using namespace std; 
 class VectorContainer : public Container{
 private:
-	vector <Base*> vectcon;
+	vector<Base*> vectcon;
 public:
-	VectroContainer(){};
+	VectorContainer(){};
 	void add_element(Base* element){vectcon.push_back(element);}
 	void print(); 
 	void sort();
--- VectorContainer_test.hpp
+++ VectorContainer_test.hpp
@@ -1,1 +1,31 @@
 //vectorcontainer test hpp
+
+#ifndef _VECTORCONTAINER_TEST_HPP_
+#define _VECTORCONTAINER_TEST_HPP_
+
+#include "VectorContainer.hpp"
+#include "base.hpp"
+#include "op.hpp"
+
+TEST(VconTest, Add_element_1)
+{
+	Op* one = new Op(1);
+	VectorContainer* test_container = new VectorContainer();
+
+	test_container->add_element(one);
+	ASSERT_EQ(test_container->size(), 1);
+	EXPECT_EQ(test_container->at(0)->evaluate(),1);
+}
+TEST(VconTest, Add_element_2)
+{
+	Op* one = new Op(1);
+	Op* two = new Op(2);
+	VectorContainer* test_container = new VectorContainer();
+	test_container->add_element(one);
+	test_container->add_element(two);
+	ASSERT_EQ(test_container->size(), 2);
+	EXPECT_EQ(test_container->at(0)->evaluate(),1);
+	EXPECT_EQ(test_container->at(1)->evaluate(),2);
+}
+
+#endif
